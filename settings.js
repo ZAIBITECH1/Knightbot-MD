480a48db43cd76383c3989ab5a8e61b1e7c263f3
ZAIBITECH1 <127785281+ZAIBITECH1@users.noreply.github.com>
2025-03-23 15:46:26 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Admin',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Admin Bot",
+  botOwner: 'ZAIBI', // Your name
+  ownerNumber: '923368683702', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
